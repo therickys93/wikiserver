77f628d490a5b8b8df8cadb6c1f155324889bc72
Riccardo Crippa <therickys93@gmail.com>
2018-04-06 18:19:03 +0200

    aggiunto variabile ambiente woeid

--- src/test/java/it/therickys93/wikiserver/WeatherUtilsTest.java
+++ src/test/java/it/therickys93/wikiserver/WeatherUtilsTest.java
@@ -58,57 +58,48 @@ public class WeatherUtilsTest {
 	
 	@Test
 	public void testStaNevischiando(){
-		// 18
 		assertEquals("sta nevischiando", WeatherUtils.getString(18));
 	}
 	
 	@Test
 	public void testSiStaAnnebbiando(){
-		// 22
 		assertEquals("si sta annebbiando", WeatherUtils.getString(22));
 	}
 	
 	@Test
 	public void testStaSoffiandoIlVento(){
-		// 24
 		assertEquals("sta soffiando il vento", WeatherUtils.getString(24));
 	}
 	
 	@Test
 	public void testSiStaRaffreddando(){
-		// 25
 		assertEquals("si sta raffreddando", WeatherUtils.getString(25));
 	}
 	
 	@Test
 	public void testSiStaAnnuvolando(){
-		// 27
 		assertEquals("si sta annuvolando", WeatherUtils.getString(27));
 	}
 	
 	@Test
 	public void testStanotteCiSonoLeNuvole(){
-		// 29
 		assertEquals("stanotte ci sono le nuvole", WeatherUtils.getString(29));
 	}
 	
 	@Test
 	public void testCiSonoNuvoleESole(){
-		// 30 44
 		assertEquals("ci sono nuvole e sole", WeatherUtils.getString(30));
 		assertEquals("ci sono nuvole e sole", WeatherUtils.getString(44));
 	}
 	
 	@Test
 	public void testStanotteNonCiSonoLeNuvole(){
-		// 31 33
 		assertEquals("stanotte non ci sono le nuvole", WeatherUtils.getString(31));
 		assertEquals("stanotte non ci sono le nuvole", WeatherUtils.getString(33));
 	}
 	
 	@Test
 	public void testIlSoleStaSplendendo(){
-		// 32 34 36
 		assertEquals("il sole sta splendendo", WeatherUtils.getString(32));
 		assertEquals("il sole sta splendendo", WeatherUtils.getString(34));
 		assertEquals("il sole sta splendendo", WeatherUtils.getString(36));
