6decf0e21936d85b0458d43d0b1709ba1d12ef41
Riccardo Crippa <therickys93@gmail.com>
2019-07-08 21:16:15 +0200

    aggiunto comando versione

--- src/main/java/it/therickys93/wikiserver/wiki/VersionCommand.java
+++ src/main/java/it/therickys93/wikiserver/wiki/VersionCommand.java
@@ -1,6 +1,8 @@
 package it.therickys93.wikiserver.wiki;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import it.therickys93.wikiapi.controller.Version;
 import it.therickys93.wikiapi.controller.WikiController;
@@ -15,8 +17,16 @@ public class VersionCommand implements Command {
 		try {
 			WikiController wikicontroller = new WikiController(Configurations.wikiControllerURL());
 			String response = wikicontroller.execute(new Version());
-			result += "Controller: " + response;
+			result += "Controller: " + response + " ";
+	        Process p = Runtime.getRuntime().exec("git describe --abbrev=0 --tags");
+	        BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	        String line; 
+	        while ((line = input.readLine()) != null)
+	        {
+	            result += "Server: " + line;
+	        }
 		} catch (IOException e){
+			e.printStackTrace();
 			result = "Errore nella versione";
 		}
 		
